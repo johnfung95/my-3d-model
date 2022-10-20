f26995438aded1961ea82a84f8acf9e9c3464878
john0810 <johnfung95@yahoo.com>
2022-10-20 15:34:17 +0800

    Add tag to side bar

--- src/UI/NavigationBar.js
+++ src/UI/NavigationBar.js
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import classes from "./NavigationBar.module.css";
 import Button from "./Button";
 const NavigationBar = ({ getMove }) => {
+  const [name, setName] = useState("Play With Me!");
+
   const getAvatarHandler = (event) => {
     event.preventDefault();
+    setName("Play With Me!");
     getMove("AVATAR");
   };
 
   const getChickenDanceHandler = (event) => {
     event.preventDefault();
+    setName("Chicken Dance!");
     getMove("CHICKEN");
   };
   const getSillyDanceHandler = (event) => {
     event.preventDefault();
+    setName("Silly Dance!");
     getMove("SILLY");
   };
 
   return (
     <div className={classes.bar}>
+      <h1>{name}</h1>
       <Button
         imageSrc={"img/avatar.png"}
         description={"Avatar"}
